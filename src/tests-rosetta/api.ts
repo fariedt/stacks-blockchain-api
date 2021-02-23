c03524daec7661dfbb92b9af247b08e93bacf6b8
Nouman Ahmad <nouman.ahmad@tintash.com>
2021-02-23 15:32:48 +0500

    Revert "fix: ecdsa signature fix"
    This reverts commit d9e7ef837dd12dff7a34adae40c61f505e310aa9.

--- src/tests-rosetta/api.ts
+++ src/tests-rosetta/api.ts
@@ -1886,7 +1886,7 @@ describe('Rosetta API', () => {
             curve_type: 'secp256k1',
           },
           signature_type: 'ecdsa_recovery',
-          hex_bytes: signature.data.slice(2) + signature.data.slice(0, 2),
+          hex_bytes: signature.data,
         },
       ],
     };
